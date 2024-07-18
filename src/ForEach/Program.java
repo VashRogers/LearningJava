ba4cf2f339a2b8ec0ded2ab8a6ea52300c8b88f3
VashRogers <rogerssj@gmail.com>
2024-07-18 13:03:02 -0400

    Criando package para trabalhar com list

--- src/ForEach/Program.java
+++ src/ForEach/Program.java
@@ -4,7 +4,6 @@ public class Program {
     public static void main(String[] args) {
         String[] vect = new String[] {"Jeff", "Daniels", "Jeremiah"};
 
-
         //Declaracao do forEach em java:
         for(String name : vect) {
             System.out.println("Nome: " + name);
---
+++ src/Lists/Program.java
@@ -0,0 +1,7 @@
+package Lists;
+
+public class Program {
+    public static void main(String[] args){
+
+    }
+}
