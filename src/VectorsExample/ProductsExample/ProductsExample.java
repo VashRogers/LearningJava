58ecf4b4f1d02462df7f242e97ca963453ea297f
VashRogers <rogerssj@gmail.com>
2024-07-18 09:26:50 -0400

    Fazendo exemplo 2 de vetores

---
+++ src/VectorsExample/ProductsExample/ProductsExample.java
@@ -0,0 +1,8 @@
+package VectorsExample.ProductsExample;
+
+public class ProductsExample {
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/VectorsExample/Program.java
+++ src/VectorsExample/Program.java
@@ -1,33 +1,11 @@
 package VectorsExample;
 
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Program {
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Trampando com vetores");
 
-        System.out.println("Digite o tamanho do vetor");
-        int n = sc.nextInt();
-        double[] vect = new double[n];
-
-        for(int i = 0; i<n; i++) {
-            System.out.println("Digite o número para armazenar no vetor");
-            vect[i] = sc.nextDouble();
-        }
-
-        double sum = 0.0;
-        for(int i = 0; i<n; i++) {
-            sum += vect[i];
-
-        }
-
-        double avg = sum/n;
-
-        System.out.println("Média de alturas: " + avg);
-
-        sc.close();
+//        HeightCalc instanciaClasse = new HeightCalc();
+//        instanciaClasse.teste();
     }
 }
