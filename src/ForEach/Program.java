17fa3397661f8fa47b2d9597a3b11cc58cd82be3
VashRogers <rogerssj@gmail.com>
2024-07-18 10:39:52 -0400

    Utilizando forEach

---
+++ src/ForEach/Program.java
@@ -0,0 +1,13 @@
+package ForEach;
+
+public class Program {
+    public static void main(String[] args) {
+        String[] vect = new String[] {"Jeff", "Daniels", "Jeremiah"};
+
+
+        //Declaracao do forEach em java:
+        for(String name : vect) {
+            System.out.println("Nome: " + name);
+        }
+    }
+}
